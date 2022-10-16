f84c77710df4ad0fce73152e67a6ae59a579a31c
ShUtOcHoK <juytyyfzvfubz@mail.ru>
2022-10-16 22:51:06 +0700

    Create model Rental

---
+++ DAL/Rental/Rental.cs
@@ -0,0 +1,13 @@
+﻿namespace RentalCar.DAL.Rental
+{
+    public class Rental
+    {
+       public int Id {get;set;}
+       public int PersonalData_Id {get;set;}
+       public int Car_Id {get;set;}
+       public DateTime FirstDate {get;set;}
+       public DateTime LastDate {get;set;}
+       public int Price {get;set;}
+       public int Discount {get;set;}
+    }
+}
