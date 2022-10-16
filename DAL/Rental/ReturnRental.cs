1309d5d7058b8a9f1ee8aba61ae0df6ccdc18074
ShUtOcHoK <juytyyfzvfubz@mail.ru>
2022-10-16 22:55:26 +0700

    Create model ReturnRental

---
+++ DAL/Rental/ReturnRental.cs
@@ -0,0 +1,10 @@
+﻿namespace RentalCar.DAL.Rental
+{
+    public class ReturnRental
+    {
+       public int Id {get;set;}
+       public int Rental_Id {get;set;}
+       public DateTime Dete {get;set;}
+       public int Price {get;set;}
+    }
+}
