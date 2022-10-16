82224795aff6c30fdec85002fc3029ea0f0dedd1
ShUtOcHoK <juytyyfzvfubz@mail.ru>
2022-10-16 22:20:08 +0700

    Create model CarStatys

---
+++ DAL/Car/CarStatys.cs
@@ -0,0 +1,10 @@
+﻿namespace RentalCar.DAL.Car
+{
+    public class CarStatys
+    {
+       public int Id {get; set;}
+       public int Car_Id {get; set;}
+       public int Name {get; set;}
+
+    }
+}
