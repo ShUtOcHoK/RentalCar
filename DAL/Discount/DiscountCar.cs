08b3b11f0c47a9f3a3d60051033c6f43026ea242
ShUtOcHoK <juytyyfzvfubz@mail.ru>
2022-10-16 22:39:54 +0700

    Create model DiscountCar

---
+++ DAL/Discount/DiscountCar.cs
@@ -0,0 +1,9 @@
+﻿namespace RentalCar.DAL.Discount
+{
+    public class DiscountCar
+    {
+        public int Id {get; set;}
+        public int Min_Year {get; set;}
+        public int Percent {get; set;}
+    }
+}
