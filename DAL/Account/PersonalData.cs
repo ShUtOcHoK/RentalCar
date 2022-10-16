346d1954dccbfa9c6ad403c73a687285b702158a
ShUtOcHoK <juytyyfzvfubz@mail.ru>
2022-10-16 21:54:34 +0700

    Create models Role and PersonalData

---
+++ DAL/Account/PersonalData.cs
@@ -0,0 +1,13 @@
+﻿namespace RentalCar.DAL.Account
+{
+    public class PersonalData
+    {
+        public int Id { get; set; }
+        public int Account_Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronomic { get; set; }
+        public string Addres { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ DAL/Account/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace RentalCar.DAL.Account
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
