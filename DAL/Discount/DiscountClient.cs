6a8cfa2d863ca9f164167e4fa882dda79a744c37
ShUtOcHoK <juytyyfzvfubz@mail.ru>
2022-10-16 22:35:44 +0700

    Create model DiscountClient

---
+++ DAL/Discount/DiscountClient.cs
@@ -0,0 +1,10 @@
+﻿namespace RentalCar.DAL.Discount
+{
+    public class DiscountClient
+    {
+        public int Id {get; set;}
+        public int Count {get; set;}
+        public int Price {get; set;}
+        public int Percent {get; set;}
+    }
+}
