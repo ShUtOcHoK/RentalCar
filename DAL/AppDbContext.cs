206dd3409165c950dce41d3fdff7e28153d86dfd
ShUtOcHoK <juytyyfzvfubz@mail.ru>
2022-10-16 23:25:56 +0700

    Create table DbContext

--- DAL/AppDbContext.cs
+++ DAL/AppDbContext.cs
@@ -1,7 +1,11 @@
 ﻿using RentalCar.DAL.Account;
+using RentalCar.DAL.Car;
+using RentalCar.DAL.Discount;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 using Microsoft.EntityFrameworkCore;
+using RentalCar.DAL.Rental;
+
 namespace RentalCar.DAL
 {
     public class AppDbContext : DbContext
@@ -11,7 +15,17 @@ namespace RentalCar.DAL
         {
             Database.EnsureCreated();
         }
+        public DbSet<Account.Account> Accounts { get; set; }
+        public DbSet<PersonalData> PersonalDatas { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Car.Car> Cars { get; set; }
+        public DbSet<CarStatys> CarStatys { get; set; }
+        public DbSet<CarType> CarTypes { get; set; }
+        public DbSet<DiscountCar> DiscountCars { get; set; }
+        public DbSet<DiscountClient> DiscountClients { get; set; }
+        public DbSet<Rental.Rental> Rentals { get; set; }
+        public DbSet<ReturnRental> ReturnRentals { get; set; }
 
-        
+
     }
 }
