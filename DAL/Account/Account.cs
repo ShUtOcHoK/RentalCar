4ae9ab9618ca0bd4de2aba3eff4707cea74bff98
ShUtOcHoK <juytyyfzvfubz@mail.ru>
2022-10-16 21:42:15 +0700

    Create model Account

---
+++ DAL/Account/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace RentalCar.DAL.Account
+{
+    public class Account
+    {
+        public int Id {get;set;}
+        public string Login {get;set;}
+        public string Password {get;set;}
+        public int Role_Id {get;set;}
+
+    }
+}
