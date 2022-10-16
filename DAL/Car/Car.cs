c33aed5c05472ede280d3f87b9a870a48b38b997
ShUtOcHoK <juytyyfzvfubz@mail.ru>
2022-10-16 22:04:13 +0700

    Create model Car

---
+++ DAL/Car/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace RentalCar.DAL.Car
+{
+    public class Car
+    {
+         public int Id {get;set;}
+         public int Name {get;set;}
+         public int Number {get;set;}
+         public int CarType_Id {get;set;}
+         public int CarStatus_Id {get;set;}
+         public int Year { get; set; }
+         public int BasePrice { get; set; }
+
+    }
+}
