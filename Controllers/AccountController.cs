0593228d77508ead788d8c0be475103a52ad50b6
ShUtOcHoK <juytyyfzvfubz@mail.ru>
2022-10-17 00:03:21 +0700

    Create AccountController

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentalCar.Models.Account;
+using RentalCar.DAL.Account;
+using RentalCar.DAL;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+
+namespace RentalCar.Controllers
+{
+    public class AccountController : Controller
+    {
+        private AppDbContext _context;
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            RegisterModel model = new RegisterModel { };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Account account = await _context.Accounts.FirstOrDefaultAsync(u => u.Login == model.Login);
+                if (account == null)
+                {
+                    // добавляем пользователя в бд
+                    account = new Account { Login = model.Login, Password = model.Password, Role_Id = 2 };
+
+                    _context.Accounts.Add(account);
+                    await _context.SaveChangesAsync();
+
+                    await Authenticate(account); // аутентификация
+
+                    return RedirectToAction("Index", "");
+                }
+                else
+                    ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+            }
+          
+            return View(model);
+        }
+
+        private IEnumerable<Role> GetRoles()
+        {
+            return _context.Roles;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Account account = await _context.Accounts
+                    .Include(u => u.Role)
+                    .FirstOrDefaultAsync(u => u.Login == model.Login && u.Password == model.Password);
+                if (account != null)
+                {
+                    await Authenticate(account); // аутентификация
+
+                    return RedirectToAction("Index", "");
+                }
+                ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+            }
+            return View(model);
+        }
+
+        private async Task Authenticate(Account account)
+        {
+            // создаем один claim
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, account.Login),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, account.Role?.Name)
+            };
+            // создаем объект ClaimsIdentity
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
+            // установка аутентификационных куки
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "");
+        }
+    }
